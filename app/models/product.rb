771e857bc3ab99ce347727970bac91783db0bf11
MaxAlmeida <llewxam100@hotmail.com>
2014-09-20 22:52:37 -0300

    exemplo

---
+++ app/models/product.rb
@@ -0,0 +1,3 @@
+class Product < ActiveRecord::Base
+  attr_accessible :nome, :price
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '69d21fc6ce91a381c937dea971bdc26b0fdc3ba9ff170151f802892429171b53b226841b3e8dad703ae23659ba88907a9688c7b8cc4620a76ee76bf5ea6d9ddb'
